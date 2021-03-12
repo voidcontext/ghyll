a871c17463ad150d5ce2174d95e08b0ac2d141a6
Gabor Pihaj <gabor.pihaj@gmail.com>
2021-03-12 11:59:20 +0000

    Rename internal trait

--- modules/core/src/main/scala/ghyll/package.scala
+++ modules/core/src/main/scala/ghyll/package.scala
@@ -1,5 +1,5 @@
 import ghyll.derivation.Derivation
 
-package object ghyll extends Derivation with Decode {
+package object ghyll extends Derivation with DecodeFunctions {
   type StreamingDecoderResult[A] = Either[StreamingDecoderError, A]
 }
