67cd3c42b0f340e27e31a8d4baf07ffc435ad8cc
Gabor Pihaj <gabor.pihaj@gmail.com>
2021-03-12 20:52:19 +0000

    Implement more encoders (simple / scalar types, list, option)

--- modules/core/src/main/scala/ghyll/Encoder.scala
+++ modules/core/src/main/scala/ghyll/Encoder.scala
@@ -1,5 +1,7 @@
 package ghyll
 
+import java.time.LocalDate
+
 import cats.instances.either._
 import cats.syntax.either._
 import cats.syntax.flatMap._
@@ -11,17 +13,38 @@ trait Encoder[A] {
 }
 
 object Encoder {
-  implicit def stringEncoder: Encoder[String] =
+  implicit val stringEncoder: Encoder[String] =
     (writer, value) => catchNonFatal(writer.value(value))
+
+  implicit val intEncoder: Encoder[Int] =
+    (writer, value) => catchNonFatal(writer.value(BigInt(value)))
+
+  implicit val booleanEncoder: Encoder[Boolean] =
+    (writer, value) => catchNonFatal(writer.value(value))
+
+  implicit val bigDecimalEncoder: Encoder[BigDecimal] =
+    (writer, value) => catchNonFatal(writer.value(value))
+
+  implicit val localDateEncoder: Encoder[LocalDate] =
+    (writer, value) => catchNonFatal(writer.value(value.toString()))
+
+  implicit def optionEncoder[A](implicit encoder: Encoder[A]): Encoder[Option[A]] =
+    (writer, value) => value.fold(catchNonFatal(writer.nullValue()))(encoder.encode(writer, _))
+
+  implicit def listEncoder[A](implicit encoder: Encoder[A]): Encoder[List[A]] =
+    (writer, value) =>
+      value.foldLeft(catchNonFatal(writer.beginArray())) { (result, elem) =>
+        result >> encoder.encode(writer, elem)
+      } >> catchNonFatal(writer.endArray())
 
   implicit def mapEncoder[A](implicit valueEncoder: Encoder[A]): Encoder[Map[String, A]] =
     (writer, value) => {
-      value.foldLeft[StreamingEncoderResult](catchNonFatal(writer.beginObject())) { case (result, (k, v)) =>
+      value.foldLeft(catchNonFatal(writer.beginObject())) { case (result, (k, v)) =>
         result >> catchNonFatal(writer.name(k)) >> valueEncoder.encode(writer, v)
       } >> catchNonFatal(writer.endObject())
     }
 
-  private def catchNonFatal[A](body: A): StreamingEncoderResult =
+  private def catchNonFatal[A](body: => A): StreamingEncoderResult =
     Either
       .catchNonFatal(body)
       .left
--- modules/core/src/test/scala/ghyll/EncoderSpec.scala
+++ modules/core/src/test/scala/ghyll/EncoderSpec.scala
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets
 import cats.effect.{IO, Resource}
 import ghyll.Generators._
 import ghyll.Utils.createWriter
-import org.scalacheck.Prop
+import org.scalacheck.{Gen, Prop}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.scalacheck.Checkers
 
@@ -69,6 +69,55 @@ class EncoderSpec extends AnyWordSpec with Checkers {
           }
         )
       }
+
+      "value is an Int" in {
+        check(
+          Prop.forAll(int) { i =>
+            testEncoder(Map("foo" -> i), s"""{"foo":${i.toString()}}""")
+          }
+        )
+      }
+
+      "value is a Boolean" in {
+        check(
+          Prop.forAll(boolean) { b =>
+            testEncoder(Map("foo" -> b), s"""{"foo":${if (b) "true" else "false"}}""")
+          }
+        )
+      }
+
+      "value is a BigDecimal" in {
+        check(
+          Prop.forAll(bigDecimal) { case (bigDecimal) =>
+            testEncoder(Map("foo" -> bigDecimal), s"""{"foo":${bigDecimal.toString()}}""")
+          }
+        )
+      }
+
+      "value is a LocalDate" in {
+        check(
+          Prop.forAll(localDate) { case (localDate) =>
+            testEncoder(Map("foo" -> localDate), s"""{"foo":"${localDate.toString()}"}""")
+          }
+        )
+      }
+
+      "value is a List" in {
+        check(
+          Prop.forAll(Gen.listOf(int)) { case (ints) =>
+            testEncoder(Map("foo" -> ints), s"""{"foo":[${ints.map(_.toString()).mkString(",")}]}""")
+          }
+        )
+      }
+
+      "value is optional" in {
+        check(
+          Prop.forAll(Gen.option(int)) { case (maybeInt) =>
+            testEncoder(Map("foo" -> maybeInt), s"""{"foo":${maybeInt.fold("null")(_.toString())}}""")
+          }
+        )
+      }
+
     }
   }
 }
