fa1459014ddfb4a8506bd7ed2e13e8690f69ecb1
Gabor Pihaj <gabor.pihaj@gmail.com>
2021-08-13 14:20:28 +0100

    Reimplement Scala 2 derived encoder instances using LazyList

--- modules/core/src/main/scala-2.13/ghyll/auto/DerivedEncoderInstances.scala
+++ modules/core/src/main/scala-2.13/ghyll/auto/DerivedEncoderInstances.scala
@@ -1,39 +1,39 @@
 package ghyll.auto
 
 // import fs2.Stream
-// import ghyll.json.JsonToken._
+import ghyll.json.JsonToken._
 // import ghyll.{Encoder, StreamingEncoderResult}
 import ghyll.{StreamingEncoderResult}
 import shapeless._
 import shapeless.labelled.FieldType
+import ghyll.Encoder
 
 private[ghyll] trait DerivedEncoderInstances {
   implicit def derivedEncoderHNil: DerivedEncoder[HNil] =
     new DerivedEncoder[HNil] {
-      def encode(value: HNil): StreamingEncoderResult = ???
-        //Right(Stream.empty)
+      def encode(value: HNil): StreamingEncoderResult =
+        Right(LazyList.empty)
     }
 
-  implicit def derivedEncoderHCons[F[_], K <: Symbol, H, T <: HList] /*(implicit
+  implicit def derivedEncoderHCons[K <: Symbol, H, T <: HList] (implicit
     witness: Witness.Aux[K],
     headEncoder: Encoder[H],
     tailEncoder: DerivedEncoder[T]
-  )*/ : DerivedEncoder[FieldType[K, H] :: T] =
+  ): DerivedEncoder[FieldType[K, H] :: T] =
     new DerivedEncoder[FieldType[K, H] :: T] {
-
-      def encode(value: FieldType[K, H] :: T): StreamingEncoderResult = ???
-        // for {
-        //   headStream <- headEncoder.encode(value.head)
-        //   tailStream <- tailEncoder.encode(value.tail)
-        // } yield Stream.emit(Key(witness.value.name)) ++ headStream ++ tailStream
+      def encode(value: FieldType[K, H] :: T): StreamingEncoderResult =
+        for {
+          headStream <- headEncoder.encode(value.head)
+          tailStream <- tailEncoder.encode(value.tail)
+        } yield Key(witness.value.name) #:: (headStream ++ tailStream)
     }
 
-  implicit def derivedEncoderGeneric[F[_], A, H] /*(implicit
+  implicit def derivedEncoderGeneric[A, H] (implicit
     lg: LabelledGeneric.Aux[A, H],
     hconsEncoder: DerivedEncoder[H]
-  )*/ : DerivedEncoder[A] =
+  ) : DerivedEncoder[A] =
     new DerivedEncoder[A] {
-      def encode(value: A): StreamingEncoderResult = ???
-        // hconsEncoder.encode(lg.to(value)).map { Stream.emit(BeginObject) ++ _ ++ Stream.emit(EndObject) }
+      def encode(value: A): StreamingEncoderResult =
+        hconsEncoder.encode(lg.to(value)).map { BeginObject #:: _ ++ LazyList(EndObject) }
     }
 }
