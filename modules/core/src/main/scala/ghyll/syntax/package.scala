7d09ec28c891fda6b4f668f88a127d58a6f0cc45
Gabor Pihaj <gabor.pihaj@gmail.com>
2021-08-13 15:59:30 +0100

    Reenable syntax shortcuts

--- modules/core/src/main/scala/ghyll/syntax/package.scala
+++ modules/core/src/main/scala/ghyll/syntax/package.scala
@@ -1,17 +1,16 @@
 package ghyll
 
-// import java.io.ByteArrayOutputStream
+import java.io.ByteArrayOutputStream
 
-// import cats.effect.{Resource, Sync}
-import cats.effect.Sync
-// import cats.syntax.functor._
+import cats.effect.{Resource, Sync}
+import cats.syntax.functor._
 
 package object syntax {
   implicit class EncodingOps[A](value: A) extends Encoding {
-    def asJsonString[F[_]: Sync](implicit encoder: Encoder[A]): F[String] = ???
-      // Resource
-      //   .fromAutoCloseable(Sync[F].delay(new ByteArrayOutputStream()))
-      //   .use(out => encode(value, out).as(out))
-      //   .map(_.toString())
+    def asJsonString[F[_]: Sync](implicit encoder: Encoder[A]): F[String] =
+      Resource
+        .fromAutoCloseable(Sync[F].delay(new ByteArrayOutputStream()))
+        .use(out => encode(value, out).as(out))
+        .map(_.toString())
   }
 }
